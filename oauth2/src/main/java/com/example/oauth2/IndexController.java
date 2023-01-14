af121c0f59db29f3e77d4ab7a0b00891e73ec7a4
junhokim <junhokim@gmarket.com>
2023-01-14 16:10:36 +0900

    OAuth 2.0 Client Fundamentals

---
+++ oauth2/src/main/java/com/example/oauth2/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.oauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
+
+}
