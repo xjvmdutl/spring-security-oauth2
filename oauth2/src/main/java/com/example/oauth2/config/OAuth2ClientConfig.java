273dd181b0b26d5ece452869f338cebdabb2d990
junhokim <junhokim@gmarket.com>
2023-02-21 23:47:45 +0900

    DefaultOAuth2AuthorizedClientManager
    - ClientCredential

--- oauth2/src/main/java/com/example/oauth2/config/OAuth2ClientConfig.java
+++ oauth2/src/main/java/com/example/oauth2/config/OAuth2ClientConfig.java
@@ -1,20 +1,12 @@
 package com.example.oauth2.config;
 
-import com.example.oauth2.CustomOAuth2AuthorizationRequestResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 
 @EnableWebSecurity
